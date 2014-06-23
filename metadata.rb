a3ca0e26967cbe049bacf8390acc272c7ad32133
Kevin Berry <kevin@opensourcealchemist.com>
2014-06-23 12:48:39 -0500

    Add metadata for chef to reference.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'cpufrequtils'
+maintainer       'Geoforce, Inc.'
+maintainer_email 'infrastructure@geoforce.com'
+license          'MIT'
+description      'Installs/Configures cpufrequtils'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+recommends "apt"
+
