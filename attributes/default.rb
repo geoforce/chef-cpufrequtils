b9e2a8c32099929126e3070107dd2e91d42959eb
Kevin Berry <kevin@opensourcealchemist.com>
2014-06-24 12:42:37 -0500

    Opps, typo.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default[:cpufrequtils][:governor] = "performance"
-default[:cpufrequitls][:enable] = "true"
+default[:cpufrequtils][:enable] = "true"
 default[:cpufrequtils][:min_speed] = "0"
 default[:cpufrequtils][:max_speed] = "0"
